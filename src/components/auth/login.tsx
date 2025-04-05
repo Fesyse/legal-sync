8b76e084f2d0138c85df29ede8710e4a8d959b03
Fesyse <maksimlol.mail.com@mail.ru>
2025-04-05 18:14:58 +0800

    translate login

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -53,8 +53,10 @@ export function Login({ className, type, ...props }: LoginFormProps) {
           <SocialButtons />
         </div>
         <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-          By clicking continue, you agree to our{" "}
-          <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+          Нажав «Продолжить» вы соглашаетесь с нашими
+          <br />
+          <a href="#">Правилами использования</a> и{" "}
+          <a href="#">Политикой конфиденциальности</a>.
         </div>
       </div>
     </div>
