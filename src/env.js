8f68d72a9d6b8cfc5fa66e1b4c862e27687fbd10
Fesyse <maksimlol.mail.com@mail.ru>
2025-04-05 15:48:43 +0800

    feat: setup sign in and sign up pages

--- src/env.js
+++ src/env.js
@@ -19,6 +19,10 @@ export const env = createEnv({
       // VERCEL_URL doesn't include `https` so it cant be validated as a URL
       process.env.VERCEL ? z.string() : z.string().url(),
     ),
+    AUTH_GOOGLE_CLIENT_ID: z.string(),
+    AUTH_GOOGLE_CLIENT_SECRET: z.string(),
+    AUTH_VK_CLIENT_ID: z.string(),
+    AUTH_VK_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -37,6 +41,10 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
     BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    AUTH_GOOGLE_CLIENT_ID: process.env.AUTH_GOOGLE_CLIENT_ID,
+    AUTH_GOOGLE_CLIENT_SECRET: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+    AUTH_VK_CLIENT_ID: process.env.AUTH_VK_CLIENT_ID,
+    AUTH_VK_CLIENT_SECRET: process.env.AUTH_VK_CLIENT_SECRET,
 
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
