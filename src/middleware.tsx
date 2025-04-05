cfaaf1a92572f81ebd650eaf5ba8170638b73536
Fesyse <maksimlol.mail.com@mail.ru>
2025-04-05 18:08:00 +0800

    fix: middleware, feat: user nav

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -2,10 +2,14 @@ import { type NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname === "/auth/sign-in") {
+    return NextResponse.next();
+  }
+
   const sessionCookie = getSessionCookie(request);
 
   if (!sessionCookie) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/auth/sign-in", request.url));
   }
 
   return NextResponse.next();
