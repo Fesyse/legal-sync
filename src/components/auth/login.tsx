c48dcaafc95e1f29b7d67b3ba80e3d39101fed42
shms0mms <shmsmms01@gmail.com>
2025-04-05 19:43:30 +0800

    fix: avatar size

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,8 +1,8 @@
+import { Icons } from "@/components/ui/icons";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { LoginForm } from "./login-form";
 import { SocialButtons } from "./social-buttons";
-import { Icons } from "@/components/ui/icons";
 
 type LoginFormProps = React.ComponentProps<"div"> & {
   type: "sign-in" | "sign-up";
@@ -46,7 +46,7 @@ export function Login({ className, type, ...props }: LoginFormProps) {
 
           <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
             <span className="bg-background text-muted-foreground relative z-10 px-2">
-              Or
+              Или
             </span>
           </div>
           <SocialButtons />
