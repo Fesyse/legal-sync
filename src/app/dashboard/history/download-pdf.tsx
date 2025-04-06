a1d77cb0f631d49229db07e219b70a4c4cc618b9
Fesyse <maksimlol.mail.com@mail.ru>
2025-04-06 10:49:19 +0800

    fix: refetch happends -> remove lazy query implementation

--- src/app/dashboard/history/download-pdf.tsx
+++ src/app/dashboard/history/download-pdf.tsx
@@ -18,12 +18,8 @@ type DownloadPDFProps = {
 };
 
 export const DownloadPDF: React.FC<DownloadPDFProps> = ({ data }) => {
-  const [trigger, fetchTechnicalSpecification] = useState(false);
   const { data: technicalSpecification } =
-    api.technicalSpecification.getById.useQuery(
-      { id: data.id },
-      { enabled: trigger },
-    );
+    api.technicalSpecification.getById.useQuery({ id: data.id });
 
   const [isConverting, setIsConverting] = useState(false);
   const [cssContent, setCssContent] = useState("");
@@ -92,7 +88,6 @@ export const DownloadPDF: React.FC<DownloadPDFProps> = ({ data }) => {
 
   const convertToPdf = async () => {
     setIsConverting(true);
-    fetchTechnicalSpecification(true);
 
     toast(<span>Начинаем создание PDF</span>, {
       description: "Пожалуйста, подождите, пока мы создаем ваш PDF...",
@@ -101,7 +96,7 @@ export const DownloadPDF: React.FC<DownloadPDFProps> = ({ data }) => {
   };
 
   useEffect(() => {
-    if (!technicalSpecification) return;
+    if (!technicalSpecification || !isConverting) return;
 
     setTimeout(async () => {
       try {
@@ -296,7 +291,7 @@ export const DownloadPDF: React.FC<DownloadPDFProps> = ({ data }) => {
         setIsConverting(false);
       }
     }, 100); // Small delay to allow UI update
-  }, [technicalSpecification]);
+  }, [technicalSpecification, isConverting]);
 
   return (
     <DropdownMenuItem onClick={convertToPdf}>Скачать в PDF</DropdownMenuItem>
